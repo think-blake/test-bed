060dd417f56361b8b3bcc347d8d92b700ceb11bc
think-blake <blake.banks@thinkcompany.com>
2022-05-20 13:19:54 -0400

    added classes

--- src/components/Nav/Nav.stories.js
+++ src/components/Nav/Nav.stories.js
@@ -3,10 +3,10 @@ const Template = args => {
   return `<div class="tsl-Nav">
  
   <ul class="tsl-Nav-list">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Not Home</a></li>
-    <li><a href="#">At the store</a></li>
-    <li><a href="#">At Bobby's House</a></li>
+    <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">Home</a></li>
+    <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">Not Home</a></li>
+    <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">At the store</a></li>
+    <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">At Bobby's House</a></li>
 
     </ul>
 
