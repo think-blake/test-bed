021ef5bca0b320294463a982844a4c40c6252038
think-blake <blake.banks@thinkcompany.com>
2022-05-26 16:20:17 -0400

    adding an image to the nav bar

--- src/components/Nav/Nav.stories.js
+++ src/components/Nav/Nav.stories.js
@@ -3,6 +3,7 @@ const Template = args => {
   return `<div class="tsl-Nav">
  
   <ul class="tsl-Nav-list">
+  <li class="tsl-Nav-list-logo"><a href="#"><img src="https://i.gifer.com/o8G.gif" alt="a rock" style="width:42px;height:42px;"></a>
     <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">Home</a></li>
     <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">Not Home</a></li>
     <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">At the store</a></li>
