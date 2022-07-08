5a3e3883d383b57749bfafde265b7321deebf5f4
think-blake <blake.banks@thinkcompany.com>
2022-07-08 12:37:02 -0400

    added a main nav wrapper

--- src/components/Nav/Nav.stories.js
+++ src/components/Nav/Nav.stories.js
@@ -1,18 +1,25 @@
 
 const Template = args => {
-  return `<div class="tsl-Nav">
- 
+  return `
+  <div class="tsl-Nav-wrapper">
+  <div class="tsl-Nav-logo-wrapper">
+ <a class="tsl-brand-logo" href="#"><img src="https://i.gifer.com/o8G.gif" alt="a rock" style="width:42px;height:42px;"></a>
+ </div>
+ <nav class="tsl-Nav">
   <ul class="tsl-Nav-list">
-  <li class="tsl-Nav-list-logo"><a href="#"><img src="https://i.gifer.com/o8G.gif" alt="a rock" style="width:42px;height:42px;"></a>
+  <li class="tsl-Nav-list-logo">
     <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">Home</a></li>
     <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">Not Home</a></li>
     <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">At the store</a></li>
     <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">At Bobby's House</a></li>
 
     </ul>
+    </nav>
 
 
-  </div>`;
+  
+  </div>
+  `;
 };
 
 export const Nav = Template.bind({});
