b4a0eba062154cf2cb35365b2fe95329afe387d5
think-blake <87831713+think-blake@users.noreply.github.com>
2022-08-02 12:33:40 -0400

    add a Navbar (#1)
    * just for testing
    
    * added typography
    
    * tried to get rid of the underlines
    
    * added classes
    
    * adding bar padding and item padding
    
    * added some new styling
    
    * added mor styling, no more underline and link colors except on hover
    
    * added padding and changed the backgroud color
    
    * adding an image to the nav bar
    
    * tried to align rock
    
    * preparing to overhaul nav bar
    
    * trying to add hover stuff
    
    * managed to get hover working
    
    * border does NOT look cool
    
    * added a main nav wrapper
    
    * i can't figure out the nav bar, saving state
    
    * preparing for PR

--- src/components/Nav/Nav.stories.js
+++ src/components/Nav/Nav.stories.js
@@ -1,17 +1,25 @@
 
 const Template = args => {
-  return `<div class="tsl-Nav">
- 
+  return `
+  <div class="tsl-Nav-wrapper">
+  <div class="tsl-Nav-logo-wrapper">
+ <a class="tsl-brand-logo" href="#"><img src="https://i.gifer.com/o8G.gif" alt="a rock" style="width:42px;height:42px;"></a>
+ </div>
+ <nav>
   <ul class="tsl-Nav-list">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Not Home</a></li>
-    <li><a href="#">At the store</a></li>
-    <li><a href="#">At Bobby's House</a></li>
+  <li class="tsl-Nav-list-logo">
+    <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">Home</a></li>
+    <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">Not Home</a></li>
+    <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">At the store</a></li>
+    <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">At Bobby's House</a></li>
 
     </ul>
+    </nav>
 
 
-  </div>`;
+  
+  </div>
+  `;
 };
 
 export const Nav = Template.bind({});
