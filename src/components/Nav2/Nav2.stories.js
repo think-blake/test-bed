85445adf8877a9be317a4f933e0accfc9e51dad9
think-blake <blake.banks@thinkcompany.com>
2022-08-08 15:30:26 -0400

    trying to add a dropdown menu. I'm not sure if I could technically make a container for the UL to make a dropdown

--- src/components/Nav2/Nav2.stories.js
+++ src/components/Nav2/Nav2.stories.js
@@ -12,14 +12,6 @@ const Template = args => {
     <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">Not Home</a></li>
     <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">At the store</a></li>
     <li class="tsl-Nav-list-item"><a class="tsl-Nav-list-link" href="#">At Bobby's House</a></li>
-    <!-- Unordered List Nesting -->
-    
-   <!-- <ul class="tsl-Nav-list-dropdown">
-    <li class="tsl-Nav-list-item"><button class="tsl-Nav-list-btn">Dropdown<i class="fa fa-caret-down"></i></button> 
-    <li class="tsl-Nav-list-drop"><a class="tsl-Nav-list-link" href="#">Option 1</a></li>
-    <li class="tsl-Nav-list-drop"><a class="tsl-Nav-list-link" href="#">Option 2</a></li>
-    <li class="tsl-Nav-list-drop"><a class="tsl-Nav-list-link" href="#">Option 3</a></li> 
-    -->
 
     </ul>
     </nav>
